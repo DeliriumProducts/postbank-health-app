1189ee1c5edc7d167598bbcc5afdcf3e81cf591d
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-06-15 21:24:42 +0300

    return from function

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
 
-exports.outputContext = functions.https.onCall((data,context) => {
- response.send(`Hello from Firebase! Your token is ${JSON.stringify(context)}`);
+exports.outputContext = functions.https.onCall((data, context) => {
+  return context;
 });
