73d687cd0bde9fa742817d98633fae4b7f7b07d4
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-06-15 21:40:56 +0300

    Change cloud function region

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 
-exports.outputContext = functions.https.onCall((data, context) => {
-  return context;
-});
+exports.outputContext = functions
+  .region('europe-west1')
+  .https.onCall((data, context) => {
+    return { context: context.auth };
+  });
